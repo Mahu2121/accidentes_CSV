c181909ac7df79ea64d77c34cf588ea0bfb621b7
Mahu2121 <hugogbmahu@gmail.com>
2025-09-29 00:11:46 +0200

    feat(AccidentesStorage): añadir metodo writeTofile

--- src/main/java/org/dam/storage/AccidentesStorageCsv.java
+++ src/main/java/org/dam/storage/AccidentesStorageCsv.java
@@ -3,6 +3,7 @@ package org.dam.storage;
 import org.dam.models.Accidente;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -53,7 +54,41 @@ public class AccidentesStorageCsv implements AccidentesStorage{
                 data[17]
         );
     }
+    @Override
+    public void writeToFile(File file, List<Accidente> accidentes) {
+        try {
+            StringBuilder content = new StringBuilder();
+            content.append("numExpediente,fecha,hora,localizacion,numero,codigoDistrito,distrito,tipoAccidente,estadoMeteorologico,tipoVehiculo,tipoPersona,rangoEdad,sexo,codLesividad,lesividad,coordenadaX,coordenadaY,positivoAlcohol\n");
+
+            for (Accidente accidente : accidentes) {
+                content.append(String.format("%d,%s,%s,%s,%d,%d,%s,%s,%s,%s,%s,%s,%s,%d,%s,%d,%d,%s\n",
+                        accidente.getNumExpediente(),
+                        accidente.getFecha(),
+                        accidente.getHora(),
+                        accidente.getLocalizacion(),
+                        accidente.getNumero(),
+                        accidente.getCodigoDistrito(),
+                        accidente.getDistrito(),
+                        accidente.getTipoAccidente(),
+                        accidente.getEstadoMeteorologico(),
+                        accidente.getTipoVehiculo(),
+                        accidente.getTipoPersona(),
+                        accidente.getRangoEdad(),
+                        accidente.getSexo(),
+                        accidente.getCodLesividad(),
+                        accidente.getLesividad(),
+                        accidente.getCoordenadaX(),
+                        accidente.getCoordenadaY(),
+                        accidente.getPositivoAlcohol()
+                ));
+            }
+            Files.writeString(file.toPath(), content.toString());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
+
 
 
 
