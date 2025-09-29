fd8c9b0c21e4645f57d4cd5770fcbd730a24c3b2
Mahu2121 <hugogbmahu@gmail.com>
2025-09-29 20:08:28 +0200

    feat(Accidente): cambiar los tipos de datos de ints a double para admitir valores especiales y crear tostring

--- src/main/java/org/dam/models/Accidente.java
+++ src/main/java/org/dam/models/Accidente.java
@@ -1,13 +1,17 @@
 package org.dam.models;
 
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Accidente {
 
-    private int numExpediente;
+    private String numExpediente;
     private String fecha;
     private String hora;
     private String localizacion;
-    private int numero;
-    private int codigoDistrito;
+    private Double numero;
+    private Double codigoDistrito;
     private String distrito;
     private String tipoAccidente;
     private String estadoMeteorologico;
@@ -15,20 +19,34 @@ public class Accidente {
     private String tipoPersona;
     private String rangoEdad;
     private String sexo;
-    private int codLesividad;
+    private Double codLesividad;
     private String lesividad;
-    private int coordenadaX;
-    private int coordenadaY;
+    private Double coordenadaX;
+    private Double coordenadaY;
     private String positivoAlcohol;
 
-    public Accidente(String fecha, int numExpediente, String hora,
-                     String localizacion, int numero, int codigoDistrito,
-                     String distrito, String tipoAccidente, String estadoMeteorologico,
-                     String tipoVehiculo, String tipoPersona, String rangoEdad,
-                     String sexo, int codLesividad, String lesividad,
-                     int coordenadaX, int coordenadaY, String positivoAlcohol) {
-        this.fecha = fecha;
+    public Accidente(
+            String numExpediente,
+            String fecha,
+            String hora,
+            String localizacion,
+            Double numero,
+            Double codigoDistrito,
+            String distrito,
+            String tipoAccidente,
+            String estadoMeteorologico,
+            String tipoVehiculo,
+            String tipoPersona,
+            String rangoEdad,
+            String sexo,
+            Double codLesividad,
+            String lesividad,
+            Double coordenadaX,
+            Double coordenadaY,
+            String positivoAlcohol
+    ) {
         this.numExpediente = numExpediente;
+        this.fecha = fecha;
         this.hora = hora;
         this.localizacion = localizacion;
         this.numero = numero;
@@ -47,76 +65,53 @@ public class Accidente {
         this.positivoAlcohol = positivoAlcohol;
     }
 
-    public String getFecha() {
-        return fecha;
+
+    public String getFecha() { return fecha; }
+    public String getNumExpediente() { return numExpediente; }
+    public String getHora() { return hora; }
+    public String getLocalizacion() { return localizacion; }
+    public Double getNumero() { return numero; }
+    public Double getCodigoDistrito() { return codigoDistrito; }
+    public String getDistrito() { return distrito; }
+    public String getTipoAccidente() { return tipoAccidente; }
+    public String getEstadoMeteorologico() { return estadoMeteorologico; }
+    public String getTipoVehiculo() { return tipoVehiculo; }
+    public String getTipoPersona() { return tipoPersona; }
+    public String getRangoEdad() { return rangoEdad; }
+    public String getSexo() { return sexo; }
+    public Double getCodLesividad() { return codLesividad; }
+    public String getLesividad() { return lesividad; }
+    public Double getCoordenadaX() { return coordenadaX; }
+    public Double getCoordenadaY() { return coordenadaY; }
+    public String getPositivoAlcohol() { return positivoAlcohol; }
+
+    public Integer getMes(String fecha) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate localDate = LocalDate.parse(fecha, formatter);
+        return localDate.getMonthValue();
     }
 
-    public int getNumExpediente() {
-        return numExpediente;
+    @Override
+    public String toString() {
+        return "Accidente{" +
+                "numExpediente='" + getNumExpediente() + '\'' +
+                ", fecha='" + getFecha() + '\'' +
+                ", hora='" + getHora() + '\'' +
+                ", localizacion='" + getLocalizacion() + '\'' +
+                ", numero=" + getNumero() +
+                ", codigoDistrito=" + getCodigoDistrito() +
+                ", distrito='" + getDistrito() + '\'' +
+                ", tipoAccidente='" + getTipoAccidente() + '\'' +
+                ", estadoMeteorologico='" + getEstadoMeteorologico() + '\'' +
+                ", tipoVehiculo='" + getTipoVehiculo() + '\'' +
+                ", tipoPersona='" + getTipoPersona() + '\'' +
+                ", rangoEdad='" + getRangoEdad() + '\'' +
+                ", sexo='" + getSexo() + '\'' +
+                ", codLesividad=" + getCodLesividad() +
+                ", lesividad='" + getLesividad() + '\'' +
+                ", coordenadaX=" + getCoordenadaX() +
+                ", coordenadaY=" + getCoordenadaY() +
+                ", positivoAlcohol='" + getPositivoAlcohol() + '\'' +
+                '}';
     }
-
-    public String getHora() {
-        return hora;
-    }
-
-    public String getLocalizacion() {
-        return localizacion;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public int getCodigoDistrito() {
-        return codigoDistrito;
-    }
-
-    public String getDistrito() {
-        return distrito;
-    }
-
-    public String getTipoAccidente() {
-        return tipoAccidente;
-    }
-
-    public String getEstadoMeteorologico() {
-        return estadoMeteorologico;
-    }
-
-    public String getTipoVehiculo() {
-        return tipoVehiculo;
-    }
-
-    public String getTipoPersona() {
-        return tipoPersona;
-    }
-
-    public String getRangoEdad() {
-        return rangoEdad;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public int getCodLesividad() {
-        return codLesividad;
-    }
-
-    public String getLesividad() {
-        return lesividad;
-    }
-
-    public int getCoordenadaX() {
-        return coordenadaX;
-    }
-
-    public int getCoordenadaY() {
-        return coordenadaY;
-    }
-
-    public String getPositivoAlcohol() {
-        return positivoAlcohol;
-    }
-
 }
